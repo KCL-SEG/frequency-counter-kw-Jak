8012ce74dd5ae84170012cbf64955552525695f2
Ka (Ka Wai) Lau <kwlau107@gmail.com>
2023-11-14 06:40:25 +0000

    Finish the code of frequency counter in frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,13 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for i in items:
+        i = str(i)
+        if i in frequencies:
+            frequencies[i] += 1
+        else:
+            frequencies[i] = 1
     return frequencies
